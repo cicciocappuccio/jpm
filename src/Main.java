7c51aea1bad2fdd8aa2aeebaf2acdd0823a35fff
cicciocappuccio <coppola.marco@infinito.it>
2016-09-03 12:34:47 +0200

    fc

--- src/Main.java
+++ src/Main.java
@@ -22,11 +22,12 @@ public class Main {
 		controller.setStocks(stocks);
 		controller.setTrades(trades);
 		
-		System.out.println("Welcome to JPM SUPER SIMPLE STOCKS exercise");
-		
+		System.out.println("Welcome to JPM SUPER SIMPLE STOCKS exercise\nIn the document of the technical test there are some things that aren't clear for me; for this reason I have doing some assumptions:\n1 - the \"Ticker Price\" can be the stock price (last formula in the document) or the price of the last trade: I assume that the ticker price is the price of the last trade of the specific stock.\n2 - In the case there aren't trade of a specific stock, what is the price of the stock? I assume that the price of the stock is the Par Value\n");
+        System.out.println("\n\n");
+
 		controller.mainMenu(mainMenu);
 
-		System.out.println("JPM SUPER SIMPLE STOCKS exercise terminated!\nBye bye!!!");
+		System.out.println("\nJPM SUPER SIMPLE STOCKS exercise terminated!\nBye bye!!!");
 	}
 
 }
